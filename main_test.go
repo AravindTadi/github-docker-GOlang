d6cdac73c08250e70112f1c0d52e808f1c75e9b5
Aravind <aravind.tadi2000@gmail.com>
2023-06-10 18:18:30 -0700

    Updated test.go file

--- main_test.go
+++ main_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 // in the pipeline we need to test
 func Test_sayHello(t *testing.T) {
-	name := "Bob"
-	want := "Hello Bob"
+	name := "Aravind"
+	want := "Hello Aravind"
 
 	if got := sayHello(name); got != want {
 		t.Errorf("hello() = %q, want %q", got, want)
